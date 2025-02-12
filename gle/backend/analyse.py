3031790eba262ab6c80bc5f5ab6293688f9ef0ef
Muhammad Fatih Abdul Aziz <wz21908@bristol.ac.uk>
2025-02-12 15:50:24 +0000

    Add FFT computation to extract natural frequencies from acceleration data

--- gle/backend/analyse.py
+++ gle/backend/analyse.py
@@ -3,6 +3,8 @@ import utils as u
 import pandas as pd
 from matplotlib import pyplot as plt
 from scipy import signal
+from scipy.fft import fft
+import numpy as np
 
 
 def get_natural_freqs(data: pd.DataFrame, options: dict):
@@ -10,10 +12,13 @@ def get_natural_freqs(data: pd.DataFrame, options: dict):
     """Get natural frequencies from acceleration data."""
     
     accelerometers = options['accelerometers']
-    peakind = signal.find_peaks_cwt(data, np.arange(1,10))
 
     # Do FFT
+    dft = fft.fft(data)
     # Pick peaks
+    peakind = signal.find_peaks_cwt(dft, np.arange(1,10))
+
+
     # Save plots with labelled peaks to images folder
     
     return
