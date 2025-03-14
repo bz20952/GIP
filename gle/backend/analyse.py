d60ce1c1a8a406af8c9381248f25f9441ab3eee5
Muhammad Fatih Abdul Aziz <wz21908@bristol.ac.uk>
2025-03-14 12:44:16 +0000

    Fixed csv calling from data folder, and pd.read syntaxing error. For Random data, parameters for peak idenfitication was determined. Need to do that for other data as well, manually. Errors still to be fixed: 1. Some noise on the imaginary part of the fft, which sometimes peaked in both direction within the same frequency band 2. Peak in Imaginary occur at slightly different frequencies accross different experiments. Right now the mode shapes are more accurate for measurement locations in the same experiments (limited to two). Suggests the dynamic of the accelerometer affects overall measurement.

--- gle/backend/analyse.py
+++ gle/backend/analyse.py
@@ -12,28 +12,27 @@ This module contains functions to analyse the data from the input file."""
 def get_natural_freqs(data: pd.DataFrame, options: dict) -> None:
 
     #Extract data
-    data = pd.read_csv('FREE_400_5.csv') #insert path to force data
-    t = data['t'].to_numpy
+    t = data['Time'].to_numpy()
 
     #If sampling frequency and duration is available, use code below
-    Fs = 1000  #insert sampling frequency
-    T = 5      #Insert sampling duration
-    N = Fs * T # Number of samples datapoints
+    #Fs = 1000  #insert sampling frequency
+    #T = 5      #Insert sampling duration
+    #N = Fs * T # Number of samples datapoints
 
     #If not, extract from csv and use code below
-    #Fs = 1/(t[1]-t[0])
-    #T = t[-1]
-    #N = len(t)
+    Fs = 1/(t[1]-t[0])
+    T = t[-1]
+    N = len(t)
 
     #extract force
-    force = data['F'].to_numpy
+    force = data['Force_1'].to_numpy()
 
     # extract responses (accelerations)
-    response_0 = data['0'].to_numpy
-    response_1 = data['l/4'].to_numpy
-    response_2 = data['l/2'].to_numpy
-    response_3 = data['3l/4'].to_numpy
-    response_4 = data['l'].to_numpy
+    response_0 = data['Accel_1'].to_numpy()
+    response_1 = data['Accel_2'].to_numpy()
+    response_2 = data['Accel_3'].to_numpy()
+    response_3 = data['Accel_4'].to_numpy()
+    response_4 = data['Accel_5'].to_numpy()
 
     # fft - rfft is used to only get the positive frequencies
     F_force = rfft(force)
@@ -53,41 +52,46 @@ def get_natural_freqs(data: pd.DataFrame, options: dict) -> None:
     FRF_3 = np.abs(F_resp_3 / F_force)
     FRF_4 = np.abs(F_resp_4 / F_force)
 
+    plt.plot(freqs, FRF_1)
+    plt.title('Frequency Response Function')
+    plt.xlabel('Frequency (Hz)')
+    plt.ylabel('Magnitude')
+    plt.show()
+
     # Find peaks in FRF to get natural frequencies, choose which measurement location to use
     # Can also use FFT to get natural frequencies
-    peaks, _ = find_peaks(FRF_1, width=50)  # Adjust width based on data
+    peaks, _ = find_peaks(FRF_1, height=45)  # For Trial Merged Random F1 10sec1, 45 height is acceptable
     natural_frequencies = freqs[peaks]
-    print(natural_frequencies)
+    #print(type(natural_frequencies))
     
-    return
+    return print("The first three natural frequencies are ", natural_frequencies[:3], "Hz")
 
 
 
 def get_mode_shapes(data= pd.DataFrame, options= dict)-> None:
     
-       #Extract data
-    data = pd.read_csv('FREE_400_5.csv') #insert path to force data
-    t = data['t'].to_numpy
+    #Extract data
+    t = data['Time'].to_numpy()
 
     #If sampling frequency and duration is available, use code below
-    Fs = 1000  #insert sampling frequency
-    T = 5      #Insert sampling duration
-    N = Fs * T # Number of samples datapoints
+    #Fs = 1000  #insert sampling frequency
+    #T = 5      #Insert sampling duration
+    #N = Fs * T # Number of samples datapoints
 
     #If not, extract from csv and use code below
-    #Fs = 1/(t[1]-t[0])
-    #T = t[-1]
-    #N = len(t)
+    Fs = 1/(t[1]-t[0])
+    T = t[-1]
+    N = len(t)
 
     #extract force
-    force = data['F'].to_numpy
+    force = data['Force_1'].to_numpy()
 
     # extract responses (accelerations)
-    response_0 = data['0'].to_numpy
-    response_1 = data['l/4'].to_numpy
-    response_2 = data['l/2'].to_numpy
-    response_3 = data['3l/4'].to_numpy
-    response_4 = data['l'].to_numpy
+    response_0 = data['Accel_1'].to_numpy()
+    response_1 = data['Accel_2'].to_numpy()
+    response_2 = data['Accel_3'].to_numpy()
+    response_3 = data['Accel_4'].to_numpy()
+    response_4 = data['Accel_5'].to_numpy()
 
     # fft - rfft is used to only get the positive frequencies
     F_force = rfft(force)
@@ -107,9 +111,9 @@ def get_mode_shapes(data= pd.DataFrame, options= dict)-> None:
     FRF_4 = np.abs(F_resp_4 / F_force)
 
     # Find peaks in FRF to get natural frequencies, choose which measurement location to use
-    peaks, _ = find_peaks(FRF_1, width=0.05)  # Adjust threshold as needed
+    peaks, _ = find_peaks(FRF_1, height=45)  # Adjust threshold as needed
     natural_frequencies = freqs[peaks]
-    print(natural_frequencies)
+    #print(natural_frequencies)
 
     # Calculating the peak of imaginary value of FRF, based on literature this is the easiest way to find mode shape experimentally
     imgFRF_0 = np.imag(F_resp_0 / F_force)
@@ -126,6 +130,16 @@ def get_mode_shapes(data= pd.DataFrame, options= dict)-> None:
 
     #Note: For random generated data, sometimes there is less than 3 detected 'peaks', which will produce error
 
+    #plt.plot(freqs, imgFRF_0, color='black')
+    #plt.plot(freqs, imgFRF_1, color='red')
+    plt.plot(freqs, imgFRF_2, color='blue')
+    plt.plot(freqs, imgFRF_3, color='green')
+    #plt.plot(freqs, imgFRF_4, color='orange')
+    plt.title('Frequency Response Function')
+    plt.xlabel('Frequency (Hz)')
+    plt.ylabel('Magnitude')
+    plt.show()
+
     #Normalizing the mode shapes, accounting for direction. Highest absolute magnitude is set to 1 and other values are scaled accordingly
     modeshape1 = ms_1/max(ms_1, key= abs)
     modeshape2 = ms_2/max(ms_2, key= abs)
@@ -133,4 +147,8 @@ def get_mode_shapes(data= pd.DataFrame, options= dict)-> None:
 
     mode_shapes = [modeshape1, modeshape2, modeshape3]
 
-    print(mode_shapes)
+    return print("Mode shape 1 is ", modeshape1, ". Mode shape 2 is ", modeshape2, ". Mode shape 3 is ", modeshape3)
+
+data = pd.read_csv(r"gle\backend\data\Trial_Merged_Random_F1_10sec1.csv")
+get_natural_freqs(data, {})
+get_mode_shapes(data, {})
