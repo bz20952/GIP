4532b89942fc59c04c1e3469ed13eed12476be2e
Muhammad Fatih Abdul Aziz <wz21908@bristol.ac.uk>
2025-03-20 16:01:43 +0000

    Analyse and plotter
    Done:
    1. Implement correct call format with data and options
    2. Created argand diagram for receptance (UNTESTED ON COLLECTED DATA)
    3. Created imaginary gain-location plot for receptance (UNTESTED ON COLLECTED DATA)
    4. Moved both to plotter
    5. Fix analyse define function sequence. Now it is in the correct order and call output from the first define function.
    
    Probably need you "George" to fix/test:
    1. Test if argand and imaginary receptance plots are visually useful for users
    2. Adjust parameter of find_peaks. I have scribbled the parameter based on the one I explored this week. Happy to give it to you but might be best to determine the type of excitation we are using
    3. IDK how to use your (data, options) input. Can you please test if I format it correctly? Thank you!
    
    Not done on my part: Finalise feedbacks in form of email. Will do it tonight.
    
    Please let me know if there is anything requiring my clarification or further action. I'll be happy to respond anytime tonight.

--- gle/backend/analyse.py
+++ gle/backend/analyse.py
@@ -10,527 +10,79 @@ import numpy as np
 This module contains functions to analyse the data from the input file."""
 
 
-
-def get_dft(data: pd.DataFrame, options: dict) -> None:
-
-    #extract force
-    force_0 = data['F0'].to_numpy()
-    force_1 = data['F1'].to_numpy()
-    force_2 = data['F2'].to_numpy()
-    force_3 = data['F3'].to_numpy()
-    force_4 = data['F4'].to_numpy()
-
-    # extract responses (accelerations)
-    response_0 = data['A0'].to_numpy()
-    response_1 = data['A1'].to_numpy()
-    response_2 = data['A2'].to_numpy()
-    response_3 = data['A3'].to_numpy()
-    response_4 = data['A4'].to_numpy()
-
-
-    # fft - rfft is used to only get the positive frequencies
-    F_force = rfft(force_0)
-    F_force_1 = rfft(force_1)
-    F_force_2 = rfft(force_2)
-    F_force_3 = rfft(force_3)
-    F_force_4 = rfft(force_4)
-
-    F_resp_0 = rfft(response_0)
-    F_resp_1 = rfft(response_1)
-    F_resp_2 = rfft(response_2)
-    F_resp_3 = rfft(response_3)
-    F_resp_4 = rfft(response_4)
-
-    return F_force, F_resp_0, F_resp_1, F_resp_2, F_resp_3, F_resp_4
-
 def get_FRF(data: pd.DataFrame, options: dict) -> None:
         #Extract data
-    t = data['t'].to_numpy()
+    for acc in accelerometers.keys():
+        if accelerometers[acc]:
+            # Compute FFT and Frequency Response Function
+            n = len(data[acc])
+            sample_rate = 1 / (data['t'][1] - data['t'][0])
+            f = np.fft.fftfreq(n, 1/sample_rate)[:n//2]  # Positive frequencies
+            fftacc = np.fft.fft(data[acc])[:n//2]
+            fftforce = np.fft.fft(data['F' + acc[1]])[:n//2]
 
-    #If sampling frequency and duration is available, use code below
-    #Fs = 1000  #insert sampling frequency
-    #T = 5      #Insert sampling duration
-    #N = Fs * T # Number of samples datapoints
-
-    #If not, extract from csv and use code below
-    Fs = 1/(t[1]-t[0])
-    T = t[-1]
-    N = len(t)
+            # Avoid division by zero
+            fftforce[np.abs(fftforce) < 1e-10] = np.finfo(float).eps
+            frf = fftacc / fftforce  # Frequency Response Function
 
      # Frequency vector
-    freqs = rfftfreq(N, 1/Fs)
+    freqs = f
 
-    # Compute FRF for each measurement point
-    FRF_0 = np.abs(F_resp_0 / F_force)
-    FRF_1 = np.abs(F_resp_1 / F_force)
-    FRF_2 = np.abs(F_resp_2 / F_force)
-    FRF_3 = np.abs(F_resp_3 / F_force)
-    FRF_4 = np.abs(F_resp_4 / F_force)
+    return freqs, fftacc, frf 
 
-    return freqs, FRF_0, FRF_1, FRF_2, FRF_3, FRF_4
+freqs, fftacc, frf = get_FRF(data, {})
 
-def plot_FRF_gain(data: pd.DataFrame, options: dict) -> None:
-     #Extract data
-    t = data['t'].to_numpy()
+def get_natural_freqs(freqs, frf) -> None:
 
-    #If sampling frequency and duration is available, use code below
-    #Fs = 1000  #insert sampling frequency
-    #T = 5      #Insert sampling duration
-    #N = Fs * T # Number of samples datapoints
+    # Find peaks in FRF to get natural frequencies, choose which measurement location to use
+    peaks, _ = find_peaks(np.abs(frf[0]), prominence=3, distance=200)
+    natural_frequencies = freqs[peaks]
 
-    #If not, extract from csv and use code below
-    Fs = 1/(t[1]-t[0])
-    T = t[-1]
-    N = len(t)
-
-    #extract force
-    force_0 = data['F0'].to_numpy()
-    force_1 = data['F1'].to_numpy()
-    force_2 = data['F2'].to_numpy()
-    force_3 = data['F3'].to_numpy()
-    force_4 = data['F4'].to_numpy()
-
-    # extract responses (accelerations)
-    response_0 = data['A0'].to_numpy()
-    response_1 = data['A1'].to_numpy()
-    response_2 = data['A2'].to_numpy()
-    response_3 = data['A3'].to_numpy()
-    response_4 = data['A4'].to_numpy()
-
-
-    # fft - rfft is used to only get the positive frequencies
-    F_force = rfft(force_0)
-
-    F_resp_0 = rfft(response_0)
-    F_resp_1 = rfft(response_1)
-    F_resp_2 = rfft(response_2)
-    F_resp_3 = rfft(response_3)
-    F_resp_4 = rfft(response_4)
-    
-    # Frequency vector
-    freqs = rfftfreq(N, 1/Fs)
-
-    # Compute FRF for each measurement point
-    FRF_0 = np.abs(F_resp_0 / F_force)
-    FRF_1 = np.abs(F_resp_1 / F_force)
-    FRF_2 = np.abs(F_resp_2 / F_force)
-    FRF_3 = np.abs(F_resp_3 / F_force)
-    FRF_4 = np.abs(F_resp_4 / F_force)
-
-    plt.plot(freqs, FRF_1)
+    #Method to cross-check the value of the natural frequencies
+    ## TO BE REMOVED AFTER TESTING ##
+    plt.plot(freqs, np.abs(frf[0]))
     plt.title('Frequency Response Function')
     plt.xlabel('Frequency (Hz)')
     plt.ylabel('Magnitude')
     plt.show()
+    
+    return natural_frequencies
 
-def inertance_to_receptance(frequency, inertance_FRF):
-    omega = 2 * np.pi * frequency  # Convert frequency to angular frequency (rad/s)
-    receptance = inertance_FRF / (-omega**2)  # Convert inertance to receptance
+def get_mode_shapes(freqs, frf)-> None:
+    # Convert inertance to receptance
+    omega = 2 * np.pi * freqs  # Convert frequency to angular frequency (rad/s)
+    frf_r = frf / (-omega**2)  # Convert inertance to receptance
     
     # Avoid division by zero at zero frequency
-    receptance[omega == 0] = np.inf
+    frf_r[omega == 0] = np.inf
 
-      #Conversion to receptance
-    rF_resp_0 = inertance_to_receptance(frequency, FRF_0)
-    rF_resp_1 = inertance_to_receptance(frequency, FRF_1)
-    rF_resp_2 = inertance_to_receptance(frequency, FRF_2)
-    rF_resp_3 = inertance_to_receptance(frequency, FRF_3)
-    rF_resp_4 = inertance_to_receptance(frequency, FRF_4)
+    abs_frf = np.abs(frf_r)
 
-    # Calculating the peak of imaginary value of receptance FRF, based on literature this is the easiest way to find mode shape experimentally
-    imgFRF_0 = np.imag(rF_resp_0 / F_force)
-    imgFRF_1 = np.imag(rF_resp_1 / F_force)
-    imgFRF_2 = np.imag(rF_resp_2 / F_force)
-    imgFRF_3 = np.imag(rF_resp_3 / F_force)
-    imgFRF_4 = np.imag(rF_resp_4 / F_force)
-
-    fig, ax = plt.subplots(nrows=3, ncols=1, figsize=(6, 9), sharex=True)
-
-    # Plot data
-    ax[0].plot(freqs, imgFRF_0, color='r')
-    ax[0].set_title("Imaginary FRF - Accelerometer 1")
-    ax[0].set_xlabel("Frequency (Hz)")
-    ax[0].set_ylabel("Response")
-
-    ax[1].plot(freqs, imgFRF_2, color='g')
-    ax[1].set_title("Imaginary FRF - Accelerometer 2")
-    ax[1].set_xlabel("Frequency (Hz)")
-    ax[1].set_ylabel("Response")
-
-    ax[2].plot(freqs, imgFRF_4, color='b')
-    ax[2].set_title("Imaginary FRF - Accelerometer 3")
-    ax[2].set_xlabel("Frequency (Hz)")
-    ax[2].set_ylabel("Response")
-
-    plt.show()
-    
-    return receptance
-
-def plot_imaginary(data: pd.DataFrame, options: dict) -> None:
-        
-   #Extract data
-    t = data['t'].to_numpy()
-
-    #If sampling frequency and duration is available, use code below
-    #Fs = 1000  #insert sampling frequency
-    #T = 5      #Insert sampling duration
-    #N = Fs * T # Number of samples datapoints
-
-    #If not, extract from csv and use code below
-    Fs = 1/(t[1]-t[0])
-    T = t[-1]
-    N = len(t)
-
-    #extract force
-    force_0 = data['F0'].to_numpy()
-    force_1 = data['F1'].to_numpy()
-    force_2 = data['F2'].to_numpy()
-    force_3 = data['F3'].to_numpy()
-    force_4 = data['F4'].to_numpy()
-
-    # extract responses (accelerations)
-    response_0 = data['A0'].to_numpy()
-    response_1 = data['A1'].to_numpy()
-    response_2 = data['A2'].to_numpy()
-    response_3 = data['A3'].to_numpy()
-    response_4 = data['A4'].to_numpy()
-
-
-    # fft - rfft is used to only get the positive frequencies
-    F_force = rfft(force_0)
-
-    F_resp_0 = rfft(response_0)
-    F_resp_1 = rfft(response_1)
-    F_resp_2 = rfft(response_2)
-    F_resp_3 = rfft(response_3)
-    F_resp_4 = rfft(response_4)
-    
-    # Frequency vector
-    freqs = np.fft.rfftfreq(N, 1/Fs)
-
-    FRF_0 = np.abs(F_resp_0 / F_force)
-    FRF_1 = np.abs(F_resp_1 / F_force)
-    FRF_2 = np.abs(F_resp_2 / F_force)
-    FRF_3 = np.abs(F_resp_3 / F_force)
-    FRF_4 = np.abs(F_resp_4 / F_force)
-
-    ## Remove this if argand plot plan is abandoned
-    FRF_0 = (F_resp_0 / F_force)
-    FRF_1 = (F_resp_1 / F_force)
-    FRF_2 = (F_resp_2 / F_force)
-    FRF_3 = (F_resp_3 / F_force)
-    FRF_4 = (F_resp_4 / F_force)
-
-    #Use this if sticking with imaginary inertance. Scale of imaginary receptance is too low
-    # Calculating the peak of imaginary value of FRF, based on literature this is the easiest way to find mode shape experimentally
-    imgFRF_0 = np.imag(F_resp_0 / F_force)
-    imgFRF_1 = np.imag(F_resp_1 / F_force)
-    imgFRF_2 = np.imag(F_resp_2 / F_force)
-    imgFRF_3 = np.imag(F_resp_3 / F_force)
-    imgFRF_4 = np.imag(F_resp_4 / F_force)
-
-
-    fig, ax = plt.subplots(nrows=3, ncols=1, figsize=(6, 9), sharex=True)
-
-    # Plot data
-    ax[0].plot(freqs, imgFRF_0, color='r')
-    ax[0].set_title("Imaginary FRF - Accelerometer 1")
-    ax[0].set_xlabel("Frequency (Hz)")
-    ax[0].set_ylabel("Response")
-
-    ax[1].plot(freqs, imgFRF_2, color='g')
-    ax[1].set_title("Imaginary FRF - Accelerometer 2")
-    ax[1].set_xlabel("Frequency (Hz)")
-    ax[1].set_ylabel("Response")
-
-    ax[2].plot(freqs, imgFRF_4, color='b')
-    ax[2].set_title("Imaginary FRF - Accelerometer 3")
-    ax[2].set_xlabel("Frequency (Hz)")
-    ax[2].set_ylabel("Response")
-
-    fig.tight_layout()
-    plt.show()
-
-    return
-
-def plot_argand(data: pd.DataFrame, options: dict) -> None:
-    #Extract data
-    t = data['t'].to_numpy()
-
-    #If sampling frequency and duration is available, use code below
-    #Fs = 1000  #insert sampling frequency
-    #T = 5      #Insert sampling duration
-    #N = Fs * T # Number of samples datapoints
-
-    #If not, extract from csv and use code below
-    Fs = 1/(t[1]-t[0])
-    T = t[-1]
-    N = len(t)
-
-    #extract force
-    force_0 = data['F0'].to_numpy()
-    force_1 = data['F1'].to_numpy()
-    force_2 = data['F2'].to_numpy()
-    force_3 = data['F3'].to_numpy()
-    force_4 = data['F4'].to_numpy()
-
-    # extract responses (accelerations)
-    response_0 = data['A0'].to_numpy()
-    response_1 = data['A1'].to_numpy()
-    response_2 = data['A2'].to_numpy()
-    response_3 = data['A3'].to_numpy()
-    response_4 = data['A4'].to_numpy()
-
-
-    # fft - rfft is used to only get the positive frequencies
-    F_force = rfft(force_0)
-
-    F_resp_0 = rfft(response_0)
-    F_resp_1 = rfft(response_1)
-    F_resp_2 = rfft(response_2)
-    F_resp_3 = rfft(response_3)
-    F_resp_4 = rfft(response_4)
-    
-    # Frequency vector
-    freqs = np.fft.rfftfreq(N, 1/Fs)
-
-    ## Remove this if argand plot plan is abandoned
-    cFRF_0 = (F_resp_0 / F_force)
-    cFRF_1 = (F_resp_1 / F_force)
-    cFRF_2 = (F_resp_2 / F_force)
-    cFRF_3 = (F_resp_3 / F_force)
-    cFRF_4 = (F_resp_4 / F_force)
-    
-    peaks, _ = find_peaks(FRF_1, prominence=3, distance=200)  # Adjust threshold as needed
-
-    arg = []
-
-    for i in range(len(peaks)):  # Loop through all peaks
-        arg_i = np.array([cFRF_0[peaks[i]], cFRF_2[peaks[i]], cFRF_4[peaks[i]]])
-        arg.append(arg_i)
-
-    # Convert list of arrays into a single NumPy array for easier processing
-    arg = np.array(arg)
-
-    fig, ax = plt.subplots()
-    ax.axhline(0, color='black', linewidth=1)
-    ax.axvline(0, color='black', linewidth=1)
-    ax.grid(True, linestyle='--', linewidth=0.5)
-    ax.set_xlabel('Real')
-    ax.set_ylabel('Imaginary')
-    ax.set_title('Argand Diagram')
-    
-    arg_peaks = arg
-    colors = ['r', 'g', 'b']  # Default colors if none provided
-    
-    for i, point in enumerate(arg_peaks):
-        color = colors[i % len(colors)]  # Cycle through colors if needed
-        plt.scatter(point.real, point.imag, color=color, s=50, label=f'Accelerometer {i*2}')
-        plt.plot([0, point.real], [0, point.imag], color=color, linestyle='-', linewidth=2)
-        plt.text(point.real, point.imag, f' {point}', fontsize=12)
-    
-    plt.legend()
-    plt.show()
-
-def get_natural_freqs(data: pd.DataFrame, options: dict) -> None:
-
-    #Extract data
-    t = data['t'].to_numpy()
-
-    #If sampling frequency and duration is available, use code below
-    #Fs = 1000  #insert sampling frequency
-    #T = 5      #Insert sampling duration
-    #N = Fs * T # Number of samples datapoints
-
-    #If not, extract from csv and use code below
-    Fs = 1/(t[1]-t[0])
-    T = t[-1]
-    N = len(t)
-
-    #extract force
-    force_0 = data['F0'].to_numpy()
-    force_1 = data['F1'].to_numpy()
-    force_2 = data['F2'].to_numpy()
-    force_3 = data['F3'].to_numpy()
-    force_4 = data['F4'].to_numpy()
-
-    # extract responses (accelerations)
-    response_0 = data['A0'].to_numpy()
-    response_1 = data['A1'].to_numpy()
-    response_2 = data['A2'].to_numpy()
-    response_3 = data['A3'].to_numpy()
-    response_4 = data['A4'].to_numpy()
-
-
-    # fft - rfft is used to only get the positive frequencies
-    F_force = rfft(force_0)
-
-    F_resp_0 = rfft(response_0)
-    F_resp_1 = rfft(response_1)
-    F_resp_2 = rfft(response_2)
-    F_resp_3 = rfft(response_3)
-    F_resp_4 = rfft(response_4)
-    
-    # Frequency vector
-    freqs = rfftfreq(N, 1/Fs)
-
-    # Compute FRF for each measurement point
-    FRF_0 = np.abs(F_resp_0 / F_force)
-    FRF_1 = np.abs(F_resp_1 / F_force)
-    FRF_2 = np.abs(F_resp_2 / F_force)
-    FRF_3 = np.abs(F_resp_3 / F_force)
-    FRF_4 = np.abs(F_resp_4 / F_force)
-
-    # Find peaks in FRF to get natural frequencies, choose which measurement location to use
-    peaks, _ = find_peaks(FRF_1, prominence=3, distance=200)
-    natural_frequencies = freqs[peaks]
-
-    # For Trial Merged Random F1 10sec1, 50 height is acceptable
-    
-    return print("Natural frequencies are", natural_frequencies, "Hz")
-
-def get_mode_shapes(data= pd.DataFrame, options= dict)-> None:
-    
-   #Extract data
-    t = data['t'].to_numpy()
-
-    #If sampling frequency and duration is available, use code below
-    #Fs = 1000  #insert sampling frequency
-    #T = 5      #Insert sampling duration
-    #N = Fs * T # Number of samples datapoints
-
-    #If not, extract from csv and use code below
-    Fs = 1/(t[1]-t[0])
-    T = t[-1]
-    N = len(t)
-
-    #extract force
-    force_0 = data['F0'].to_numpy()
-    force_1 = data['F1'].to_numpy()
-    force_2 = data['F2'].to_numpy()
-    force_3 = data['F3'].to_numpy()
-    force_4 = data['F4'].to_numpy()
-
-    # extract responses (accelerations)
-    response_0 = data['A0'].to_numpy()
-    response_1 = data['A1'].to_numpy()
-    response_2 = data['A2'].to_numpy()
-    response_3 = data['A3'].to_numpy()
-    response_4 = data['A4'].to_numpy()
-
-
-    # fft - rfft is used to only get the positive frequencies
-    F_force = rfft(force_0)
-
-    F_resp_0 = rfft(response_0)
-    F_resp_1 = rfft(response_1)
-    F_resp_2 = rfft(response_2)
-    F_resp_3 = rfft(response_3)
-    F_resp_4 = rfft(response_4)
-    
-    # Frequency vector
-    freqs = np.fft.rfftfreq(N, 1/Fs)
-
-    FRF_0 = np.abs(F_resp_0 / F_force)
-    FRF_1 = np.abs(F_resp_1 / F_force)
-    FRF_2 = np.abs(F_resp_2 / F_force)
-    FRF_3 = np.abs(F_resp_3 / F_force)
-    FRF_4 = np.abs(F_resp_4 / F_force)
-
-    ## Remove this if argand plot plan is abandoned
-    cFRF_0 = (F_resp_0 / F_force)
-    cFRF_1 = (F_resp_1 / F_force)
-    cFRF_2 = (F_resp_2 / F_force)
-    cFRF_3 = (F_resp_3 / F_force)
-    cFRF_4 = (F_resp_4 / F_force)
-
-    #Use this if sticking with imaginary inertance. Scale of imaginary receptance is too low
-    # Calculating the peak of imaginary value of FRF, based on literature this is the easiest way to find mode shape experimentally
-    imgFRF_0 = np.imag(F_resp_0 / F_force)
-    imgFRF_1 = np.imag(F_resp_1 / F_force)
-    imgFRF_2 = np.imag(F_resp_2 / F_force)
-    imgFRF_3 = np.imag(F_resp_3 / F_force)
-    imgFRF_4 = np.imag(F_resp_4 / F_force)
-
-
-    fig, ax = plt.subplots(nrows=3, ncols=1, figsize=(6, 9), sharex=True)
-
-    # Plot data
-    ax[0].plot(freqs, imgFRF_0, color='r')
-    ax[0].set_title("Imaginary FRF - Accelerometer 1")
-    ax[0].set_xlabel("Frequency (Hz)")
-    ax[0].set_ylabel("Response")
-
-    ax[1].plot(freqs, imgFRF_2, color='g')
-    ax[1].set_title("Imaginary FRF - Accelerometer 2")
-    ax[1].set_xlabel("Frequency (Hz)")
-    ax[1].set_ylabel("Response")
-
-    ax[2].plot(freqs, imgFRF_4, color='b')
-    ax[2].set_title("Imaginary FRF - Accelerometer 3")
-    ax[2].set_xlabel("Frequency (Hz)")
-    ax[2].set_ylabel("Response")
-
-    fig.tight_layout()
-
-    plt.show()
+    i_frf_r0 = np.imag(frf_r[0])
+    i_frf_r1 = np.imag(frf_r[1])
+    i_frf_r2 = np.imag(frf_r[2])
 
     ### ARGAND DIAGRAM###
     # Find peaks in FRF to get natural frequencies, choose which measurement location to use
-    peaks, _ = find_peaks(FRF_1, prominence=3, distance=200)  # Adjust threshold as needed
-
-
-    arg = []
-
-    for i in range(len(peaks)):  # Loop through all peaks
-        arg_i = np.array([cFRF_0[peaks[i]], cFRF_2[peaks[i]], cFRF_4[peaks[i]]])
-        arg.append(arg_i)
-
-    # Convert list of arrays into a single NumPy array for easier processing
-    arg = np.array(arg)
-
-    fig, ax = plt.subplots()
-    ax.axhline(0, color='black', linewidth=1)
-    ax.axvline(0, color='black', linewidth=1)
-    ax.grid(True, linestyle='--', linewidth=0.5)
-    ax.set_xlabel('Real')
-    ax.set_ylabel('Imaginary')
-    ax.set_title('Argand Diagram')
-    
-    arg_peaks = arg[1]
-    colors = ['r', 'g', 'b']  # Default colors if none provided
-    
-    for i, point in enumerate(arg_peaks):
-        color = colors[i % len(colors)]  # Cycle through colors if needed
-        plt.scatter(point.real, point.imag, color=color, s=50, label=f'Accelerometer {i*2}')
-        plt.plot([0, point.real], [0, point.imag], color=color, linestyle='-', linewidth=2)
-        plt.text(point.real, point.imag, f' {point}', fontsize=12)
-    
-    plt.legend()
-    plt.show()
-
+    peaks, _ = find_peaks(abs_frf[0], prominence=3, distance=200)  # Adjust threshold as needed
 
     #START PRODUCING MODESHAPE LIST
     ms = []  # Initialize an empty list to store ms_i arrays
 
     for i in range(len(peaks)):  # Loop through all peaks
-        ms_i = np.array([imgFRF_0[peaks[i]], imgFRF_2[peaks[i]], imgFRF_4[peaks[i]]])
+        ms_i = np.array([i_frf_r0[peaks[i]], i_frf_r1[peaks[i]], i_frf_r2[peaks[i]]])
     
         # Normalize by dividing by the maximum absolute value in ms_i
         max_abs_value = np.max(np.abs(ms_i)) if np.any(ms_i) else 1  # Avoid division by zero
         ms_i = ms_i / max_abs_value
     
         ms.append(ms_i)
-
+    
     # Convert list of arrays into a single NumPy array for easier processing
-    ms = np.array(ms)
+    modeshape = np.array(ms)
 
     # Print results
-    for idx, m in enumerate(ms, start=1):
+    for idx, m in enumerate(modeshape, start=1):
         print(f"ms_{idx}", m)
 
-    return ms
-
-data = pd.read_csv(r"gle\backend\data\STEPPED_2.csv") 
-get_natural_freqs(data, {})
-mode_shape = get_mode_shapes(data, {})
+    return modeshape
